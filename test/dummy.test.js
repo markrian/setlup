fb59dd003399175d6539099704b798e8a944760e
Mark Florian <markrian@gmail.com>
2015-08-16 17:39:58 +0000

    set up testing with karma

---
+++ test/dummy.test.js
@@ -0,0 +1,12 @@
+describe('dummy', function () {
+  var Example = require('app');
+
+  it('works at all', function () {
+    expect(true).toBe(true);
+  });
+
+  it('works at all', function () {
+    expect(1).toBe(1);
+  });
+
+});
