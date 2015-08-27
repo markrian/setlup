a3c9310f994f390615bd245304f5bb545317c6b2
Mark Florian <markrian@gmail.com>
2015-08-27 23:05:05 +0000

    use chai for assertions

--- test/unit/dummy.test.js
+++ test/unit/dummy.test.js
@@ -1,24 +1,26 @@
-import assert from 'assert';
+import chai from 'chai';
 import Example from '../../app/app';
+
+let expect = chai.expect
 
 describe('dummy', () => {
   it('works at all', () => {
-    assert.equal(true, true);
+    expect(true).to.be.true;
   });
 
   it('works at all', () => {
-    assert.strictEqual(1, 1);
+    expect(1).to.equal(1);
   });
 
   it('imports as I expect it to', () => {
     var example = new Example();
     var value = {};
-    assert.strictEqual(example.run(value), value);
+    expect(example.run(value)).to.equal(value);
   });
 
   it('imports as I expect it to', () => {
     var example = new Example();
-    assert.equal(example.add(1, 2), 3);
+    expect(example.add(1, 2)).to.equal(3);
   });
 
 });
