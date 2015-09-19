918cc631e52bdecb8c669f0f40bf5b32076adfbf
Mark Florian <markrian@gmail.com>
2015-09-19 20:13:42 +0100

    Add test for adding transactions and resolving a simple transaction.

--- test/unit/transactions.test.js
+++ test/unit/transactions.test.js
@@ -15,9 +15,37 @@ describe('Transactions', () => {
     assert.deepEqual(transactions.list, []);
   });
 
+  it('can add transactions which show up in the list member', () => {
+    let transaction = {
+      creditor: 'foo',
+      amount: 1234.56,
+      debtors: ['foo', 'bar'],
+    };
+    transactions.add(transaction);
+    assert.deepEqual(transactions.list, [transaction]);
+  });
+
   it('resolves to empty list without any transactions', () => {
     let resolution = transactions.getResolution();
     assert.deepEqual(resolution, []);
+  });
+
+  it('resolves one transaction to its inverse', () => {
+    let transaction = {
+      creditor: 'foo',
+      amount: 1234.56,
+      debtors: ['foo', 'bar'],
+    };
+    transactions.add(transaction);
+
+    assert.deepEqual(
+      transactions.getResolution(),
+      {
+        creditor: 'bar',
+        amount: 1234.56 / 2,
+        debtors: 'foo',
+      }
+    );
   });
 
 });
