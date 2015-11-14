963f50b51ff80e97e035f3e386f2280e62c84de6
Mark Florian <markrian@gmail.com>
2015-11-14 15:22:30 +0000

    tests pass

--- src/transactions.js
+++ src/transactions.js
@@ -4,6 +4,18 @@ class Transactions {
     this.list = [];
   }
 
+  add(...transactions) {
+    this.list.push(...transactions);
+  }
+
+  getPeople() {
+    let people = [];
+    this.list.forEach((transaction) => {
+      people.push(transaction.creditor, ...transaction.debtors);
+    })
+    return new Set(people);
+  }
+
   getResolution() {
     return [];
   }
--- test/unit/transactions.test.js
+++ test/unit/transactions.test.js
@@ -3,18 +3,18 @@ import assert from 'assert';
 import Transactions from '../../src/transactions';
 
 
-describe('Transactions', () => {
+describe('Transactions', function () {
   let transactions;
 
-  beforeEach(() => {
+  beforeEach(function () {
     transactions = new Transactions();
   });
 
-  it('has a list member', () => {
+  it('has a list member', function () {
     assert.deepEqual(transactions.list, []);
   });
 
-  it('can add transactions which show up in the list member', () => {
+  it('can add transactions which show up in the list member', function () {
     let transaction = {
       creditor: 'foo',
       amount: 1234.56,
@@ -22,14 +22,40 @@ describe('Transactions', () => {
     };
     transactions.add(transaction);
     assert.deepEqual(transactions.list, [transaction]);
+
+    let transactions_ = [{
+      creditor: 'foo',
+      amount: 1234.56,
+      debtors: ['biz', 'bar'],
+    }, {
+      creditor: 'baz',
+      amount: 1,
+      debtors: ['bar', 'foo']
+    }];
+    transactions.add(...transactions_);
+    assert.deepEqual(transactions.list, [transaction, ...transactions_]);
   });
 
-  it('resolves to empty list without any transactions', () => {
+  it('can list the people involved in all transactions', function () {
+    let transactions_ = [{
+      creditor: 'foo',
+      amount: 1234.56,
+      debtors: ['foo', 'bar'],
+    }, {
+      creditor: 'baz',
+      amount: 1,
+      debtors: ['bar', 'foo']
+    }];
+    transactions.add(...transactions_);
+    assert.deepEqual(['foo', 'bar', 'baz'], [...transactions.getPeople()]);
+  });
+
+  it('resolves to empty list without any transactions', function () {
     let resolution = transactions.getResolution();
     assert.deepEqual(resolution, []);
   });
 
-  it('resolves one transaction to its inverse', () => {
+  it('resolves one transaction to its inverse', function () {
     let transaction = {
       creditor: 'foo',
       amount: 1234.56,
