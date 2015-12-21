79da03227fa43598a1ac6054d759712f1fe5a337
Mark Florian <markrian@gmail.com>
2015-12-21 14:59:26 +0000

    RationalNumbers now properly simplify themselves.

--- src/rational-number.js
+++ src/rational-number.js
@@ -1,12 +1,54 @@
 export default class RationalNumber {
 
     constructor(numerator = 0, denominator = 1) {
-        this.numerator = numerator;
-        this.denominator = denominator;
+        let simplified = simplify(numerator, denominator);
+        Object.assign(this, simplified);
     }
 
     valueOf() {
         return this.numerator / this.denominator;
     }
 
+    static fromNumber(float) {
+        let numerator, denominator, factor
+        if (float === parseInt(float)) {
+            factor = 1;
+        } else {
+            let asString = float.toString();
+            let numDecimals = asString.split('.')[1].length;
+            factor = Math.pow(10, numDecimals);
+        }
+
+        return new RationalNumber(float * factor, factor);
+    }
+
+}
+
+/**
+ * @param {number} numerator
+ * @param {number} denominator
+ * @returns {Object} Simplified { numerator, denominator }
+ */
+function simplify(numerator, denominator) {
+    var commonDivisor = gcd(numerator, denominator);
+
+    if (numerator < 0 !== denominator < 0) {
+        numerator = -Math.abs(numerator);
+        denominator = Math.abs(denominator);
+        commonDivisor = Math.abs(commonDivisor);
+    }
+
+    return {
+        numerator: numerator / commonDivisor,
+        denominator: denominator / commonDivisor,
+    }
+}
+
+/**
+ * gcd(a, 0) = a
+ * gcd(a, b) = gcd(b, a mod b)
+ */
+function gcd(a, b) {
+    if (b === 0) return a;
+    return gcd(b, a % b);
 }
--- test/unit/rational-number.test.js
+++ test/unit/rational-number.test.js
@@ -6,42 +6,49 @@ import RationalNumber from '../../src/rational-number';
 
 describe('RationalNumber', function () {
 
-    function makeRational(n) {
-        return new RationalNumber(n);
-    }
-
-    it('can be instantiated and defaults to zero', function () {
-        let r = makeRational();
-
+    it('can be instantiated', function () {
+        let r = new RationalNumber(0, 1);
         assert.strictEqual(r.numerator, 0);
         assert.strictEqual(r.denominator, 1);
         assert.strictEqual(r.valueOf(), 0);
     });
 
+    it(`throws when not instantiated with 'new'`, function () {
+        assert.throws(() => RationalNumber(0, 1));
+    });
+
+    it('simplifies itself', function () {
+        let tests = [
+            [[100, 50], [2, 1]],
+            [[100, 16], [25, 4]],
+            [[100, 17], [100, 17]],
+            [[528, 840], [22, 35]],
+            [[-50, 12], [-25, 6]],
+            [[50, -12], [-25, 6]],
+            [[-50, -12], [25, 6]],
+        ];
+
+        tests.forEach(test => {
+            let r = new RationalNumber(test[0][0], test[0][1]);
+            assert.strictEqual(r.numerator, test[1][0]);
+            assert.strictEqual(r.denominator, test[1][1]);
+            assert.strictEqual(r.valueOf(), test[0][0] / test[0][1]);
+        });
+    });
+
     it('can be instantiated with a number', function () {
         let value = 2;
-        let r = makeRational(value);
+        let r = RationalNumber.fromNumber(value);
 
         assert.strictEqual(r.numerator, value);
         assert.strictEqual(r.denominator, 1);
         assert.strictEqual(r.valueOf(), value);
     });
 
-    it('can be instantiated with a string', function () {
-        let value = "2";
-        let r = makeRational(value);
-
-        assert.strictEqual(r.numerator, Number(value));
-        assert.strictEqual(r.denominator, 1);
-        assert.strictEqual(r.valueOf(), Number(value));
-    });
-
-    it('can be instantiated with a numerator and denominator');
-
     it('can be instantiated with a instance of RationalNumber');
 
     it('can be added to a regular number', function () {
-        assert.strictEqual(makeRational(1.2) + 2.3, 3.5);
+        assert.strictEqual(new RationalNumber() + 2.3, 2.3);
     });
 
 });
