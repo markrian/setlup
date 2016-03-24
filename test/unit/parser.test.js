3fb533b3ead55b3f123e041bf46956809e71d5c7
Mark Florian <markrian@gmail.com>
2016-03-24 20:29:16 +0000

    add excess whitespace test

--- test/unit/parser.test.js
+++ test/unit/parser.test.js
@@ -8,4 +8,9 @@ describe('parser', function () {
         assert.deepEqual(parseLine('gary spent 114.20'),
             { creditor: 'gary', amount: 114.2, debtors: ['*'] });
     });
+
+    it('can parse a simple line with excess whitespace', function () {
+        assert.deepEqual(parseLine('  gary   spent   114.20  '),
+            { creditor: 'gary', amount: 114.2, debtors: ['*'] });
+    });
 });
