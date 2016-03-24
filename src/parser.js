fb18431527cface63d413f2c1d964c330b98a345
Mark Florian <markrian@gmail.com>
2016-03-24 20:41:02 +0000

    pass the test

--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,16 @@
 export function parseLine(line) {
     const spent = 'spent';
+    const for_ = 'for';
+
     const spentPos = line.indexOf(spent);
     const creditor = line.slice(0, spentPos).trim();
     const amount = parseFloat(line.slice(spentPos + spent.length));
 
-    return { creditor, amount, debtors: ['*'] }
+    const forPos = line.indexOf(for_);
+    let debtors = ['*'];
+    if (forPos > -1) {
+        debtors = line.slice(forPos + for_.length).split(',').map(d => d.trim());
+    }
+
+    return { creditor, amount, debtors }
 }
