acf55389571ea0157f7b369681d5e3f744e88c00
Mark Florian <markrian@gmail.com>
2016-03-24 20:36:17 +0000

    add test for parsing creditors

--- test/unit/parser.test.js
+++ test/unit/parser.test.js
@@ -13,4 +13,9 @@ describe('parser', function () {
         assert.deepEqual(parseLine('  gary   spent   114.20  '),
             { creditor: 'gary', amount: 114.2, debtors: ['*'] });
     });
+
+    it('can parse a simple line with debtors', function () {
+        assert.deepEqual(parseLine('gary spent 400 for peter, mike, lucy'),
+            { creditor: 'gary', amount: 400, debtors: ['peter', 'mike', 'lucy'] });
+    });
 });
