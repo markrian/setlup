945c93777329767fab28e39b3206009ea5d9acc6
Mark Florian <markrian@gmail.com>
2016-03-24 20:28:06 +0000

    this simple test passes!

--- src/parser.js
+++ src/parser.js
@@ -1,1 +1,8 @@
-export function parseLine() {}
+export function parseLine(line) {
+    const spent = 'spent';
+    const spentPos = line.indexOf(spent);
+    const creditor = line.slice(0, spentPos).trim();
+    const amount = parseFloat(line.slice(spentPos + spent.length));
+
+    return { creditor, amount, debtors: ['*'] }
+}
