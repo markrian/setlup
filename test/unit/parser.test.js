0bf14ec5c1bdcbfd499e71323dd3c03c453e77b4
Mark Florian <markrian@gmail.com>
2018-11-08 17:30:40 +0800

    refactor parser tests

--- test/unit/parser.test.js
+++ test/unit/parser.test.js
@@ -2,51 +2,47 @@ import { parseLine } from '../../src/parser';
 
 
 describe('parser', function () {
-    test('can parse a simple line', function () {
-        expect(parseLine('gary spent 114.20')).toEqual(
-            { creditor: 'Gary', amount: 114.2, debtors: ['*'] });
+    const validLines = [{
+        description: 'can parse a simple line',
+        line: 'gary spent 114.20',
+        expected: { creditor: 'Gary', amount: 114.2, debtors: ['*'] },
+    }, {
+        description: 'can parse a simple line with excess whitespace',
+        line: '  gary   spent   114.20  ',
+        expected: { creditor: 'Gary', amount: 114.2, debtors: ['*'] },
+    }, {
+        description: 'can parse a simple line with debtors',
+        line: 'gary spent 400 for peter, mike, lucy',
+        expected: { creditor: 'Gary', amount: 400, debtors: ['Peter', 'Mike', 'Lucy'] },
+    }, {
+        description: 'can parse a simple line with a misleadingly named creditor',
+        line: 'fordspenth spent 114.20',
+        expected: { creditor: 'Fordspenth', amount: 114.2, debtors: ['*'] },
+    }, {
+        description: 'should ignore duplicate debtors',
+        line: 'foo spent 10 for bar, qux, bar',
+        expected: { creditor: 'Foo', amount: 10, debtors: ['Bar', 'Qux'] },
+    }, {
+        description: `should normalise peoples' names`,
+        line: 'foo spent 10 for Foo, qux  yo, BAZ-FOO man',
+        expected: { creditor: 'Foo', amount: 10, debtors: ['Foo', 'Qux Yo', 'Baz-Foo Man']},
+    }];
+
+    validLines.forEach(({ description, line, expected }) => {
+        test(description, () => {
+            expect(parseLine(line)).toEqual(expected);
+        });
     });
 
-    test('it throws an error on a malformed line', function () {
-        expect(function () {
-            parseLine('nonsense goes here, foo');
-        }).toThrow('Parsing error');
-    });
+    const invalidLines = [
+        ['nonsense', 'nonsense goes here, foo'],
+        ['no spent keyword', 'a did 4'],
+        ['invalid number', 'a spent 4xyz'],
+    ];
 
-    test('it throws an error on a malformed line', function () {
-        expect(function () {
-            parseLine('a did 4');
-        }).toThrow('Parsing error');
-    });
-
-    test('can parse a simple line with excess whitespace', function () {
-        expect(parseLine('  gary   spent   114.20  ')).toEqual(
-            { creditor: 'Gary', amount: 114.2, debtors: ['*'] });
-    });
-
-    test('can parse a simple line with debtors', function () {
-        expect(parseLine('gary spent 400 for peter, mike, lucy')).toEqual(
-            { creditor: 'Gary', amount: 400, debtors: ['Peter', 'Mike', 'Lucy'] });
-    });
-
-    test('can parse a simple line with a misleadingly named creditor', function () {
-        expect(parseLine('fordspenth spent 114.20')).toEqual(
-            { creditor: 'Fordspenth', amount: 114.2, debtors: ['*'] });
-    });
-
-    test('should ignore duplicate debtors', function () {
-        expect(parseLine('foo spent 10 for bar, qux, bar')).toEqual(
-            { creditor: 'Foo', amount: 10, debtors: ['Bar', 'Qux'] });
-    });
-
-    test(`should normalise peoples' names`, function () {
-        expect(parseLine('foo spent 10 for Foo, qux  yo, BAZ-FOO man')).toEqual(
-            { creditor: 'Foo', amount: 10, debtors: ['Foo', 'Qux Yo', 'Baz-Foo Man']});
-    });
-
-    test('should throw on invalid number', function () {
-        expect(function () {
-            parseLine('a spent 4xyz');
-        }).toThrow('Parsing error');
+    invalidLines.forEach(([description, line]) => {
+        test(`line with ${description} should throw a parse error`, () => {
+            expect(() => parseLine(line)).toThrow('Parsing error');
+        });
     });
 });
